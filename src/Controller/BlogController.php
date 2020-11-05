d16f3986fc3e7864752e799528d1bd91599289f8
Lateuh <alexy.lefevre@gmail.com>
2020-11-05 18:39:35 +0100

    tp1

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'nom_page' => 'Accueil'
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/post/{id}", name="post")
+     */
+    public function post(int $id): Response
+    {
+        return $this->render('blog/post.html.twig', [
+            'id' => $id
+        ]);
+    }
+}
