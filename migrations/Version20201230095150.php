c3c6c211cefa355bf0248945b0b4b0de4e166de6
Lateuh <alexy.lefevre@gmail.com>
2020-12-30 11:08:34 +0100

    avec psql

--- migrations/Version20201230095150.php
+++ migrations/Version20201230095150.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201229110407 extends AbstractMigration
+final class Version20201230095150 extends AbstractMigration
 {
     public function getDescription() : string
     {
