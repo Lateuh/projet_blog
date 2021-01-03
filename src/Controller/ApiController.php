bfdbaf2e6fcc4a34cd72b76e19813c85a8bb2e41
Lateuh <alexy.lefevre@gmail.com>
2021-01-03 12:42:00 +0100

    ajout api rest

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,6 +6,7 @@ use App\Service\SerializationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiController extends AbstractController
